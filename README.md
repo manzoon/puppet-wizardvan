de48fe2524cbd971d7a48bf65b405cc8fad9830b
John Kinsella <jlk@stratosec.co>
2015-11-15 20:01:27 -0800

    Initial checkin.

---
+++ README.md
@@ -0,0 +1,89 @@
+# Puppet-Wizardvan
+
+Installs and manages the "Wizardvan" (AKA sensu-metrics-relay) Sensu metrics relay
+
+the open source monitoring framework [Sensu](http://sensuapp.org).
+[![Puppet Forge](http://img.shields.io/puppetforge/v/sensu/sensu.svg)](https://forge.puppetlabs.com/sensu/sensu)
+
+## Installation
+
+    $ puppet module install jlk/puppet-wizardvan
+
+## Prerequisites
+
+- Running Sensu server
+
+### Dependencies
+
+- [puppetlabs/stdlib](https://github.com/puppetlabs/puppetlabs-stdlib)
+- [puppetlabs-vcsrepo](https://github.com/puppetlabs/puppetlabs-vcsrepo)
+- [sensu-puppet](https://github.com/sensu/sensu-puppet) 
+
+See `metadata.json` for details.
+
+## Basic example
+
+    node 'sensu-server.foo.com' {
+      class { 'wizardvan':;}
+      ...
+    }
+
+## Parameters:
+
+###`wv_dest`
+
+String. Location to download wizardvan to from github
+Default: /usr/local/lib/sensu-metrics-relay
+
+###`graphite_relay`
+
+Boolean. Specifies if data should be relayed to a graphite server
+Default: true
+
+###`graphite_host`
+
+String. Graphite server hostname or IP address
+Default: localhost
+
+###`graphite_port`
+
+Integer. Port on graphite host to connect to
+Default: 2003
+
+###`graphite_max_queue_size`
+
+Integer. Maximum queue size for sending to graphite
+Default: 16384
+
+###`tsdb_relay`
+
+Boolean. Specifies if data should be relayed to an openTSDB server
+Default: false
+
+###`tsdb_host`
+
+String. OpenTSDB server hostname or IP address
+Default: localhost
+
+###`tsdb_port`
+
+Integer. Port on OpenTSDB host to connect to
+Default: 4424
+
+## License
+
+   Copyright 2015 John Kinsella
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   See LICENSE file for more details.
---
+++ manifests/init.pp
@@ -0,0 +1,94 @@
+#   Copyright 2015 John Kinsella
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# = Class: wizardvan
+#
+# Installs and configures the Sensu metrics relay WizardVan
+#
+# == Parameters
+#
+# [*wv_dest*]
+#   String. Location to download wizardvan to from github
+#   Default: /usr/local/lib/sensu-metrics-relay
+#
+# [*graphite_relay*]
+#   Boolean. Specifies if data should be relayed to a graphite server
+#   Default: true
+#
+# [*graphite_host*]
+#   String. Graphite server hostname or IP address
+#   Default: localhost
+#
+# [*graphite_port*]
+#   Integer. Port on graphite host to connect to
+#   Default: 2003
+#
+# [*graphite_max_queue_size*]
+#   Integer. Maximum queue size for sending to graphite
+#   Default: 16384
+#
+# [*tsdb_relay*]
+#   Boolean. Specifies if data should be relayed to an openTSDB server
+#   Default: false
+#
+# [*tsdb_host*]
+#   String. OpenTSDB server hostname or IP address
+#   Default: localhost
+#
+# [*tsdb_port*]
+#   Integer. Port on OpenTSDB host to connect to
+#   Default: 4424
+#
+class wizardvan (
+  $graphite_relay          = true,
+  $graphite_host           = 'localhost',
+  $graphite_port           = 2003,
+  $graphite_max_queue_size = 16384,
+  $tsdb_relay              = false,
+  $tsdb_host               = 'localhost',
+  $tsdb_port               = 4424,
+  $wv_dest                 = '/usr/local/lib/sensu-metrics-relay'
+) {
+  validate_bool($graphite_relay, $tsdb_relay)
+  if !is_integer($graphite_port) { fail('graphite_port must be an integer') }
+  if !is_integer($graphite_max_queue_size) { fail('graphite_max_queue_size must be an integer') }
+  if !is_integer($tsdb_port) { fail('tsdb_port must be an integer') }
+
+  vcsrepo { $wv_dest:
+    ensure    => present,
+    provider  => git,
+    source    => 'https://github.com/opower/sensu-metrics-relay.git',
+    require   => [File['/etc/sensu/conf.d/config_relay.json'],Package['git']],
+    notify    => Exec['copywizardvanfiles'];
+  }
+
+  exec {
+    # We don't have to restart sensu after this, as the sensu
+    # module will do so after fixing ownership and permissions.
+    'copywizardvanfiles':
+      command     => "cp -R ${wv_dest}/lib/sensu/extensions/* /etc/sensu/extensions",
+      refreshonly => true,
+      path        => '/bin',
+  }
+  if $::bios_version == 'VirtualBox' {
+    $config_relay_template = "/tmp/vagrant-puppet/modules-f2a3e880375508488722eafa8258d2eb/wizardvan/templates/config.json.erb"
+  } else {
+    $config_relay_template = 'puppet:///modules/wizardvan/config.json.erb'
+  }
+
+  file { '/etc/sensu/conf.d/config_relay.json':
+    content => template($config_relay_template),
+    require => Class['sensu'];
+  }
+}
